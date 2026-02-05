d5cb30a3bcd13504e26e424700d1b584258216a6
artarona <artaronacorreo@gmail.com>
2026-02-04 22:26:03 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -161,11 +161,8 @@ def generar_listado_propiedades(propiedades):
     
     for i, prop in enumerate(propiedades[:10], 1):  # Limitar a 10 propiedades
         # listado += f"{numero_a_emoji(i)} {prop.get('titulo', 'Sin título')}\n"
-        listado += (
-                f"{numero_a_emoji(i)} {prop.get('titulo', 'Sin título')}\n"
-                f"   🏷️ Operación: {prop.get('operacion', 'Sin operación')}\n"
-        )
-        
+        listado += f"   🏷️ Operación: {prop.get('operacion', 'Sin operación')}\n"
+        listado += f"{numero_a_emoji(i)} {prop.get('titulo', 'Sin título')}\n"
         listado += f"   📍 {prop.get('barrio', 'N/A')} | "
         listado += f"💰 "
         precio = prop.get('precio', 0)
