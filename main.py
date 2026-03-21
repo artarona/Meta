e38f719315601df894bd622a4c14200217e8081c
artarona <artaronacorreo@gmail.com>
2026-03-21 00:01:02 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2852,5 +2852,8 @@ if __name__ == "__main__":
     
     print("=" * 60 + "\n")
     
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    
+    if __name__ == "__main__":
+        app.run(debug=False)
+    # port = int(os.environ.get("PORT", 10000))
+    # app.run(host="0.0.0.0", port=port, debug=False)
