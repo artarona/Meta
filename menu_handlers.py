6103a95b6d0ce1ada992c1df14444961d86d6299
artarona <artaronacorreo@gmail.com>
2026-04-19 11:33:35 -0300

    Add files via upload

--- menu_handlers.py
+++ menu_handlers.py
@@ -373,8 +373,8 @@ def _nav_listado_buttons():
     return WhatsAppResponse.buttons(
         # header="📍 NAVEGAR",
         # body="¿Qué querés hacer?",
-        header="",
-        body="",
+        header=" ",
+        body=" ",
         buttons=[
             {"id": "m", "title": "Volver al menú"},
             {"id": "s", "title": "Salir"}
